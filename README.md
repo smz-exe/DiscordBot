67f534341cf9cc5a7980264d091c784d7080003b
smz-exe <shimizu-yuki13861@keio.jp>
2024-08-20 13:26:24 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Nyquist -AttendanceRecord
+
+
+朝活部の、朝活部による、朝活部のための出席管理bot
+
+Done is better than perfect.
---
+++ models/testScores.mjs
@@ -0,0 +1,34 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import sequelize from './sequelize.mjs';
+import User from './user.mjs';
+
+const TestScore = sequelize.define('TestScore', {
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  score: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  totalQuestions: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  normalizedScore: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  }
+}, {
+  timestamps: false
+});
+
+// Userモデルとの関連付け
+TestScore.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(TestScore, { foreignKey: 'userId' });
+
+export default TestScore;
